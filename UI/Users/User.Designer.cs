208873b04a2c45704b21fbf3b421f74760816041
huyvinh0605 <huyvinh199@gmail.com>
2025-04-14 18:15:10 +0700

    sua Form_initialchoice

--- UI/Users/User.Designer.cs
+++ UI/Users/User.Designer.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:UI/Users/User.Designer.cs
 ﻿namespace Internet_Cafe_Manager_App.UI.Users
 {
     partial class User
@@ -36,4 +37,44 @@
 
         #endregion
     }
+=======
+﻿namespace Internet_Cafe_Manager_App.UI
+{
+    partial class Form_InitialChoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+    }
+>>>>>>> Stashed changes:UI/Form_InitialChoice.Designer.cs
 }
